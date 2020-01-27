4b32d988314ff7b437f67aef8425d8043a9a87c6
BradleyG248 <calvinandhobbs28@gmail.com>
2020-01-27 15:18:27 -0700

    made a me object

---
+++ app.js
@@ -0,0 +1,19 @@
+let me = {
+  firstName: "Bradley",
+  lastName: "Graham",
+  age: "19",
+  favorite: {
+    food: "lasagna",
+    color: "maroon"
+  },
+  pets: {
+    Marlow: {
+      age: 7,
+      color: "spotted black"
+    },
+    George: {
+      age: 3938,
+      color: "beige"
+    }
+  }
+}
